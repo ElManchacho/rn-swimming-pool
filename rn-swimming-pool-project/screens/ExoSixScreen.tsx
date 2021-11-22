ca7385483d7e846442cfe5e73c3bbc7d3b06d352
ElManchacho <pld.pro@hotmail.com>
2021-11-22 16:23:40 +0100

    stuckOnSix

--- rn-swimming-pool-project/screens/ExoSixScreen.tsx
+++ rn-swimming-pool-project/screens/ExoSixScreen.tsx
@@ -1,24 +1,16 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { Button, FlatList, ScrollView, StyleSheet, TextInput } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
-export default function ExoSixScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Exo Six</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/ExoSixScreen.tsx" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({ //adding style
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'rgba(128,128,128,0.1)',
   },
   title: {
     fontSize: 20,
@@ -29,4 +21,62 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+  align:{
+    alignContent:'flex-start',
+    flexDirection:'row',
+    backgroundColor: 'rgba(128,128,128,0.01)',
+  }
 });
+
+
+
+class List extends React.Component { //creating class
+
+
+  addToList = () => this.setState(
+    prevState => ({ ...prevState, list:this.state.list.push({}), text:"Adding"})
+  )
+
+  change = () => this.setState(
+    prevState => ({...prevState, text:this.state.text+"Add"})
+  )
+  
+  state = { 
+    list: [{key:"Test"},{key:"Oui"}],
+    text:""
+  };
+
+  render() {
+    const { list } = this.state;
+    return (
+      <View style={styles.container}>
+        <SafeAreaView style={styles.align}>
+          <TextInput
+            onChange={this.change}
+            />
+          <Button
+            onPress={this.addToList}
+            title="Add"
+            color="rgb(33,150,243)"/>
+        </SafeAreaView>
+        <View>
+        <FlatList
+        data={list}
+        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+      />
+      </View>
+      </View>
+    );
+  }
+}
+
+const ExoSixScreen = () => ( //putting class into tag
+  <List />
+);
+
+export default ExoSixScreen; //exporting the newly created tag
